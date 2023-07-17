c3f66e23130d279a473b2bbe09631d02755fa2f4
j52k8snow <104110251+j52k8snow@users.noreply.github.com>
2023-07-17 15:58:03 +0800

    Create Logger.py

---
+++ Logger.py
@@ -0,0 +1,36 @@
+class Logger:
+    def __init__(self, name, trigger=None) -> None:
+        self.name = name
+        self.trigger = trigger
+
+    def print(self, *x, **kwargs):
+        if self.trigger is not None:
+            self.trigger({
+                'type': 'print',
+                'name': self.name,
+                'args': x,
+                'kwargs': kwargs
+            })
+            return
+        emit('log_message',
+             {'type': 'print', 'name': self.name, 'message': ' '.join(map(str, x)), 'kwargs': kwargs})
+
+    def imshow(self, img):
+        if self.trigger is not None:
+            self.trigger({
+                'type': 'imshow',
+                'name': self.name,
+                'args': img
+            })
+            return
+        emit('log_message', {'type': 'imshow', 'name': self.name, 'image': img})
+
+    @classmethod
+    def get_logger(cls, name):
+        logger = cls(name, trigger=Logger.global_trigger)
+        cls.loggers.setdefault(id(logger), logger)
+        return logger
+
+
+Logger.loggers = {}
+Logger.global_trigger = None
